54b6ec8de7e4d0e5046227d539b8aa7aaa5e8a6f
ritwikdiv <ritwik.edu@gmail.com>
2021-04-16 23:12:32 -0400

    components for client

---
+++ client/src/components/AppNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mx-auto p-8 sm:flex-grow md:flex sm:overflow items-center">
+    <div class="sm:w-1/2 md:w-1/4 flex-auto text-xl">
+      <h1 class="text-indigo-dark title">Hoop</h1>
+    </div>
+    <div class="sm:w-1/2 md:w-1/4 flex-auto mt-4">
+      <router-link class="font-bold no-underline mr-5" to="/">
+        <button
+          class="font-bold py-1 px-2 hover:bg-grey-darkest hover:text-white rounded-lg transform motion-safe:hover:scale-110"
+        >
+          <font-awesome-icon :icon="['fas', 'globe-asia']" class="mr-2" />
+          Memories
+        </button></router-link
+      >
+      <router-link class="font-bold no-underline ml-5 mr-1" to="/notes"
+        ><button
+          class="font-bold py-1 px-2 hover:bg-grey-darkest hover:text-white rounded-lg transform motion-safe:hover:scale-110"
+        >
+          <font-awesome-icon :icon="['fas', 'quote-right']" class="mr-2" />
+          Notes
+        </button></router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
+.title {
+  font-family: "Lobster", cursive;
+}
+</style>
---
+++ client/src/views/Notes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppNavbar />
+    <h1>This is the notes page</h1>
+  </div>
+</template>
+
+<script>
+import AppNavbar from "../components/AppNavbar.vue";
+export default {
+  name: "Notes",
+  components: { AppNavbar },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>  
