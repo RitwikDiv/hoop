f5f492c45756ed4b6196932ab261496af8f75f68
ritwikdiv <ritwik.edu@gmail.com>
2021-04-16 23:12:17 -0400

    some client stuff

---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import "@/assets/styles/main.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas, far);
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
