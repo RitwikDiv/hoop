0eb49670be52b49593dd421185e12174ddb00287
ritwikdiv <ritwik.edu@gmail.com>
2021-04-18 21:06:13 -0400

    untested notes logic

--- routes/notes.js
+++ routes/notes.js
@@ -1,51 +1,60 @@
 // Importing necessary packages
 const express = require('express');
 const Joi = require('joi');
-const mongoose = require("mongoose");
-const auth = require("../middleware/auth");
+const mongoose = require('mongoose');
+const auth = require('../middleware/auth');
+const { Notes, validateNote } = require('../models/note');
 
 // Initializing router
 const router = express.Router();
 
 // API Routes
 router.get('/', auth, async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+	try {
+		const notes = await Notes.find({
+			author: mongoose.Types.ObjectId(req.user._id),
+		});
+		res.send(notes);
+	} catch (err) {
+		res.status(500).send('Internal Server Error');
+	}
 });
 
 router.get('/:id', auth, async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+	try {
+		const note = await Notes.findById(req.params.id);
+		if (!note) return res.status(404).send('Note not found');
+		res.send(note);
+	} catch (err) {
+		res.status(500).send('Internal Server Error');
+	}
 });
 
 router.post('/', auth, async (req, res) => {
-    try {
+	try {
+		const { error } = validateNote(_.pick(req.body, ['desc']));
+		if (error) return res.status(400).send(error.details[0].message);
 
-    } catch (err) {
+		const note = new Note({
+			desc: req.body.desc,
+			author: mongoose.Types.ObjectId(req.user._id),
+		});
+		await note.save();
 
-    }
-});
-
-router.put('/:id', auth, async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+		res.send(note);
+	} catch (err) {
+		res.status(500).send('Internal server error');
+	}
 });
 
 router.delete('/:id', auth, async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+	try {
+		const note = await Notes.findByIdAndDelete(req.params.id);
+		if (!note) return res.status(404).send(`Note could not be found`);
+		res.send(note);
+	} catch (err) {
+		res.status(500).send('Internal server error');
+	}
 });
 
 module.exports = router;
