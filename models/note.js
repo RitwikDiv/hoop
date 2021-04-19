ea56f5825fc8237ed36d279d7ad96035b0e2ee92
ritwikdiv <ritwik.edu@gmail.com>
2021-04-18 21:05:32 -0400

    models reformatting

--- models/note.js
+++ models/note.js
@@ -1,29 +1,37 @@
 // Importing necessary packages
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 // Creating a model
-const Notes = mongoose.model("Note", mongoose.Schema({
-    desc: {
-        type: String,
-        required: true,
-        minLength: 10,
-        maxLength: 400
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
-}));
+const Notes = mongoose.model(
+	'Note',
+	mongoose.Schema({
+		desc: {
+			type: String,
+			required: true,
+			trim: true,
+			minLength: 10,
+			maxLength: 400,
+		},
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+		},
+		created_at: {
+			type: Date,
+			default: Date.now,
+		},
+	})
+);
 
 // Create a joi validation schema function
 function validateNote(note) {
-    const schema = Joi.object({
-        desc: Joi.string().min(10).max(400).required(),
-    });
-    return schema.validate(note);
+	const schema = Joi.object({
+		desc: Joi.string().min(10).max(400).required(),
+	});
+	return schema.validate(note);
 }
-
 
 exports.Notes = Notes;
 exports.validateNote = validateNote;
--- models/user.js
+++ models/user.js
@@ -1,67 +1,63 @@
 // Importing necessary packages
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Joi = require('joi');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // Setting up environment variables
-require("dotenv").config();
+require('dotenv').config();
 
-const passwordComplexity = require("joi-password-complexity");
+const passwordComplexity = require('joi-password-complexity');
 const complexityOptions = {
-    min: 5,
-    max: 250,
-    lowerCase: 1,
-    upperCase: 1,
-    numeric: 1,
-    symbol: 1,
-    requirementCount: 4,
+	min: 5,
+	max: 250,
+	lowerCase: 1,
+	upperCase: 1,
+	numeric: 1,
+	symbol: 1,
+	requirementCount: 4,
 };
 
 // Creating a user model
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 2,
-        maxLength: 100
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: 5,
-        maxLength: 255
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 1024
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 2,
+		maxLength: 100,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		minLength: 5,
+		maxLength: 255,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 5,
+		maxLength: 1024,
+	},
 });
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: this.id}, process.env.HOOP_JWTPRIVATEKEY);
-    return token;
+// Add more fields if interested
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this.id }, process.env.HOOP_JWTPRIVATEKEY);
+	return token;
 };
 
 const User = mongoose.model('User', userSchema);
 
-
 // Create a joi validation schema function
 function validateUser(user) {
-    const schema = Joi.object({
-        name: Joi.string().min(2).max(100).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: passwordComplexity(complexityOptions)
-    });
-    return schema.validate(user);
+	const schema = Joi.object({
+		name: Joi.string().min(2).max(100).required(),
+		email: Joi.string().min(5).max(255).required().email(),
+		password: passwordComplexity(complexityOptions),
+	});
+	return schema.validate(user);
 }
-
 
 exports.User = User;
 exports.validateUser = validateUser;
