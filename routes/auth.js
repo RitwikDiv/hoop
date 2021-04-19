f6c17088d093b02161ce26e7c487fd5e8b364f65
ritwikdiv <ritwik.edu@gmail.com>
2021-04-18 21:05:56 -0400

    reformatted basic routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,47 @@
 // Importing necessary packages
 const express = require('express');
 const Joi = require('joi');
-const mongoose = require("mongoose");
-const { User, passwordComplexity, complexityOptions } = require("../models/user.js");
-const _ = require("lodash");
-const bcrypt = require("bcrypt");
+const {
+	User,
+	passwordComplexity,
+	complexityOptions,
+} = require('../models/user.js');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 // Initializing router
 const router = express.Router();
 
 // Auth Validation
 function validate(req) {
-    const schema = Joi.object({
-        email: Joi.string().min(5).max(255).required().email(),
-        password: passwordComplexity(complexityOptions)
-    });
-    return schema.validate(req);
+	const schema = Joi.object({
+		email: Joi.string().min(5).max(255).required().email(),
+		password: passwordComplexity(complexityOptions),
+	});
+	return schema.validate(req);
 }
 
 // API Routes
 router.post('/', async (req, res) => {
-    try {
-        const { error } = validate(_.pick(req.body, ["email", "password"]));
-        if (error) return res.status(400).send(error.details[0].message);
+	try {
+		const { error } = validate(_.pick(req.body, ['email', 'password']));
+		if (error) return res.status(400).send(error.details[0].message);
 
-        let user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(400).send('Invalid User/Password');
+		let user = await User.findOne({ email: req.body.email });
+		if (!user) return res.status(400).send('Invalid User/Password');
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        if (!validPassword) return res.status(400).send("Invalid User/Password");
-        
-        const token = user.generateAuthToken();
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!validPassword) return res.status(400).send('Invalid User/Password');
 
-        res.send(token);
-    } catch (err) {
-        res.status(500).send("Server error during authentication");
-    }
+		const token = user.generateAuthToken();
+
+		res.send(token);
+	} catch (err) {
+		res.status(500).send('Server error during authentication');
+	}
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,68 +1,57 @@
 // Importing necessary packages
 const express = require('express');
 const Joi = require('joi');
-const mongoose = require("mongoose");
-const { User, validateUser } = require("../models/user.js");
-const _ = require("lodash");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const { User, validateUser } = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
 
 // Setting up environment variables
-require("dotenv").config();
+require('dotenv').config();
 
 // Initializing router
 const router = express.Router();
 
 // _.pick from array
 function project(...properties) {
-    return function project(object, index, array) {
-        return _.pick(object, ...properties);
-    };
+	return function project(object, index, array) {
+		return _.pick(object, ...properties);
+	};
 }
 
 // API Routes
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.send(_.map(users, project("_id", "name", "email")));
-    } catch (err) {
-        res.status(500).send("Server error during query");
-    }
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+router.get('/me', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).select('-password');
+		res.send(user);
+	} catch (err) {
+		res.status(500).send('Server error during user query');
+	}
 });
 
 router.post('/', async (req, res) => {
-    try {
-        const { error } = validateUser(_.pick(req.body, ["name", "email", "password"]));
-        if (error) return res.status(400).send(error.details[0].message);
+	try {
+		const { error } = validateUser(
+			_.pick(req.body, ['name', 'email', 'password'])
+		);
+		if (error) return res.status(400).send(error.details[0].message);
 
-        let user = await User.findOne({ email: req.body.email });
-        if (user) return res.status(400).send('User already registered.');
+		let user = await User.findOne({ email: req.body.email });
+		if (user) return res.status(400).send('User already registered.');
 
-        user = new User(_.pick(req.body, ["name", "email", "password"]));
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password,salt);
-        await user.save();
+		user = new User(_.pick(req.body, ['name', 'email', 'password']));
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(user.password, salt);
+		await user.save();
 
-        const token = user.generateAuthToken();
-        res.header('x-auth-token',token).send(_.pick(user, ["_id", "name", "email"]));
-    } catch (err) {
-        res.status(500).send("Server error during registration");
-    }
-});
-
-router.put('/:id', async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
+		const token = user.generateAuthToken();
+		res
+			.header('x-auth-token', token)
+			.send(_.pick(user, ['_id', 'name', 'email']));
+	} catch (err) {
+		res.status(500).send('Server error during registration');
+	}
 });
 
 module.exports = router;
