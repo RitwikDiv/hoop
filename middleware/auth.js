ee1462a42a5ff90bf3b26c51b0c43dc603414d1f
ritwikdiv <ritwik.edu@gmail.com>
2021-04-18 21:04:36 -0400

    reformatting auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,17 @@
 // Importing necessary packages
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // Setting up environment variables
-require("dotenv").config();
+require('dotenv').config();
 
-module.exports = function auth(req,res,next) {
-    const token = req.header('x-auth-token');
-    if (!token) return res.status(401).send("Access Denied. No token provided!");
-    try {
-        const decoded = jwt.verify(token, process.env.HOOP_JWTPRIVATEKEY);
-        req.user = decoded;
-        next();
-    } catch (ex){
-        res.status(400).send('Invalid token');
-    }   
-}
+module.exports = function auth(req, res, next) {
+	const token = req.header('x-auth-token');
+	if (!token) return res.status(401).send('Access Denied. No token provided!');
+	try {
+		const decoded = jwt.verify(token, process.env.HOOP_JWTPRIVATEKEY);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		res.status(400).send('Invalid token');
+	}
+};
