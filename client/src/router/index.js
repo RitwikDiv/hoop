ff4f16644fab2da25ec0951c17820b8cc8d6bd01
ritwikdiv <ritwik.edu@gmail.com>
2021-04-20 21:51:34 -0400

    added new component routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,39 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Memories from '../views/Memories.vue'
-import Notes from '../views/Notes.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Memories from '../views/Memories.vue';
+import Notes from '../views/Notes.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Memories',
-    component: Memories
-  },
-  {
-    path: '/notes',
-    name: 'Notes',
-    component: Notes
-  }
-]
+	{
+		path: '/',
+		redirect: '/login',
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: Register,
+	},
+	{
+		path: '/memories',
+		name: 'Memories',
+		component: Memories,
+	},
+	{
+		path: '/notes',
+		name: 'Notes',
+		component: Notes,
+	},
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+	history: createWebHashHistory(),
+	routes,
+});
 
-export default router
+export default router;
