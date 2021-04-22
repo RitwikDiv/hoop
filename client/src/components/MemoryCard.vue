7abce1d37a97c6744eb73a8ebe8036c1831cc868
ritwikdiv <ritwik.edu@gmail.com>
2021-04-21 20:57:53 -0400

    basic memory card formatting

---
+++ client/src/components/MemoryCard.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="bg-grey-lightest p-3 rounded-lg mb-2 mt-2" v-bind:id="id">
+		<p class="mt-2 text-xl font-extrabold">
+			{{ title }}
+		</p>
+		<p class="mt-4 text-md font-medium tracking-normal leading-loose">
+			{{ desc }}
+		</p>
+		<div class="mt-4 mb-2 flex flex-row align-middle justify-between">
+			<div class="meta">
+				<p class="text-sm font-bold align-middle">
+					<span class="date mr-3">{{ date_string }}</span> ●
+					<span class="ml-3 location">{{ place_name }}</span>
+				</p>
+			</div>
+			<div class="options align-middle">
+				<button
+					class="mr-3 focus:outline-none p-1 rounded-lg hover:bg-indigo-darkest hover:text-grey-lightest"
+				>
+					<font-awesome-icon :icon="['fas', 'pen']" class="text-md" />
+				</button>
+				<button
+					class="ml-3 mr-2 p-1 rounded-lg hover:bg-indigo-darkest hover:text-grey-lightest"
+				>
+					<font-awesome-icon :icon="['fas', 'trash-alt']" class="text-md" />
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MemoryCard',
+	props: ['id', 'title', 'desc', 'place_name', 'date_string'],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- client/src/views/Memories.vue
+++ client/src/views/Memories.vue
@@ -1,48 +1,19 @@
 <template>
 	<div class="h-screen flex">
 		<AppNavbar />
-		<div class="flex-1 ml-5 flex overflow-hidden">
+		<div
+			class="md:w-2/5 bg-grey-darkest flex flex-col justify-center m-5 rounded-lg sm:invisible md:visible"
+			id="map"
+		/>
+		<div class="flex-1 ml-5 flex overflow-hidden m-5 mr-10">
 			<div class="flex-1 text-left overflow-y-scroll">
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
-				<h1>asdfasdfasdf</h1>
+				<h1 class="title text-4xl text-blue mb-10">Memories</h1>
+				<MemoryCard
+					title="Valentine's Day 2020 🍝"
+					place_name="Maggiano's Little Italy"
+					date_string="February 5, 2020"
+					desc="We went on a lovely date to Maggianos Little Italy and got some delicious pasta. The waitress sat us next to the kitchen which was a bit annoying but you got to sit in a spot where you could see a little baby playing!"
+				/>
 			</div>
 		</div>
 	</div>
@@ -50,12 +21,19 @@
 
 <script>
 import AppNavbar from '../components/AppNavbar.vue';
+import MemoryCard from '../components/MemoryCard.vue';
 export default {
 	name: 'Memories',
 	components: {
 		AppNavbar,
+		MemoryCard,
 	},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+.title {
+	font-family: 'Lobster', cursive;
+}
+</style>
