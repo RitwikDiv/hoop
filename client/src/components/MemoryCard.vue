d6a2f4dd73b2624502844c5c363e95b503ad301e
ritwikdiv <ritwik.edu@gmail.com>
2021-05-11 23:15:19 -0400

    formats date better

--- client/src/components/MemoryCard.vue
+++ client/src/components/MemoryCard.vue
@@ -9,13 +9,16 @@
 		<div class="mt-4 mb-2 flex flex-row flex-wrap align-middle justify-between">
 			<div class="meta">
 				<p class="text-sm font-bold align-middle">
-					<span class="date mr-1">{{ date_string }}</span> ●
+					<span class="date mr-1">{{
+						new Date(date_string).toDateString()
+					}}</span>
+					●
 					<span class="ml-1 location">{{ place_name }}</span>
 				</p>
 			</div>
 			<div class="options align-middle">
 				<button
-					@click="toggleEditing()"
+					@click="deleteCard()"
 					class="mr-3 focus:outline-none p-2 rounded-lg hover:bg-indigo-darkest hover:text-grey-lightest"
 				>
 					<font-awesome-icon :icon="['fas', 'trash']" class="text-md" />
@@ -42,7 +45,7 @@ export default {
 		};
 	},
 	methods: {
-		editMemory() {},
+		deleteCard() {},
 	},
 };
 </script>
