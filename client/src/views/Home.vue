2421e7664bcf10f9bd0cf7f81db767ab4747f4f0
ritwikdiv <ritwik.edu@gmail.com>
2021-05-13 22:27:59 -0400

    finished homepage

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,14 +4,15 @@
 		<div class="p-2 ml-10 flex flex-col w-full m-auto">
 			<div class="flex flex-row justify-center mb-10">
 				<div class="w-1/2 m-auto">
-					<p class="text-left text-4xl font-extrabold font-serif">
+					<p class="text-left text-5xl font-extrabold font-serif">
 						Welcome to Hoop Boop!
 					</p>
-					<p class="font-medium text-md leading-loose text-left mt-5">
+					<p class="font-medium text-lg leading-loose text-left mt-5">
 						As life goes on and we make millions of memories, it is hard to lose
-						track of the ones that matter. This app hopes to helps recollect the
-						most cherished moments and affirmations you have received through
-						your lifetime. Go out there and make some wonderful memories!
+						track of the ones that matter. This app hopes to help you recollect
+						the most cherished moments and affirmations you have received
+						through your lifetime. Go out there and make some wonderful
+						memories!
 					</p>
 				</div>
 				<div class="w-1/2">
@@ -28,12 +29,12 @@
 					<div class="flex flex-row justify-start p-2 mt-5 ml-2">
 						<font-awesome-icon
 							:icon="['fas', 'globe-asia']"
-							class="text-xl mr-10"
+							class="text-xl mr-5"
 						/>
-						<p class="font-bold text-lg">Cherish your memories!</p>
+						<p class="font-bold text-xl">Cherish your memories!</p>
 					</div>
 					<div class="mt-3 ml-2">
-						<p class="font-medium leading-loose text-left p-2">
+						<p class="font-medium leading-loose text-left text-lg p-2">
 							Click on the globe icon in the menu to save your precious memories
 							and their locations on the map! Don't be afraid to reach out and
 							ask people to hang out!
@@ -44,12 +45,12 @@
 					<div class="flex flex-row justify-start p-2 mt-5 ml-2">
 						<font-awesome-icon
 							:icon="['fas', 'layer-group']"
-							class="text-xl mr-10"
+							class="text-xl mr-5"
 						/>
-						<p class="font-bold text-lg">Save your notes!</p>
+						<p class="font-bold text-xl">Save your notes!</p>
 					</div>
 					<div class="mt-3 ml-2">
-						<p class="font-medium leading-loose text-left p-2">
+						<p class="font-medium leading-loose text-left text-lg p-2">
 							Click on the notes icon in the menu to save all the wonderful
 							messages, and compliments you have recieved! Don't forget to
 							compliment wonderful people in your life!
@@ -69,6 +70,9 @@ export default {
 	name: 'Home',
 	components: {
 		AppNavbar,
+	},
+	created() {
+		if (localStorage.jwtToken === undefined) this.$router.push('/login');
 	},
 	data() {
 		return {
